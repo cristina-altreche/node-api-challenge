0f24fb3d8da4669cdddf53a7bd90c3630e3494e4
Cristina Altreche <c.altreche@gmail.com>
2020-08-07 11:40:18 -0400

    projectsRouter.js ready to code in. Server 8000 up and running

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const morgan = require("morgan")
-const helmet = require("helmet")
+const projectsRouter = require("./projects/projectsRouter");
+const morgan = require("morgan");
+const helmet = require("helmet");
 const server = express();
 
 server.use(express.json());
-server.use(helmet())
+server.use(helmet());
+server.use(logger);
 
 server.get("/", (req, res) => {
-    const message = process.env.MESSAGE || "hello from the fallback message";
-    res.status(200).json({message, database: process.env.DB_NAME})
-})
+  const message = process.env.MESSAGE || "hello from the fallback message";
+  res.status(200).json({ message, database: process.env.DB_NAME });
+});
+
+//CUSTOM MIDDLEWARE
+///LOGGER
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+  next();
+}
+
+server.use("/api/projects", projectsRouter);
+
+module.exports = server;
